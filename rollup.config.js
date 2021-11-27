120e3c87d10a1ea1348f7bfbd3907909a763e86f
Ringo Hoffmann <riho@live.de>
2021-11-27 18:02:18 +0100

    init

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from 'rollup-plugin-typescript2';
+import url from '@rollup/plugin-url';
+import pkg from './package.json';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    url({
+      include: ['**/*.woff2', '**/*.woff', '**/*.ttf'],
+      destDir: 'dist/fonts',
+      emitFiles: true,
+    }),
+    typescript({
+      clean: true,
+      tsconfig: 'tsconfig-rollup.json',
+      typescript: require('typescript'),
+    }),
+  ],
+};
