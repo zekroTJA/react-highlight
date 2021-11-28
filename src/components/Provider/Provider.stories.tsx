6a751e856abc2e05cf986ca2c014df0315a4d203
Ringo Hoffmann <riho@live.de>
2021-11-28 10:32:27 +0100

    update provider storybook

--- src/components/Provider/Provider.stories.tsx
+++ src/components/Provider/Provider.stories.tsx
@@ -13,10 +13,24 @@ export default {
   },
 } as ComponentMeta<typeof Provider>;
 
+const content =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend metus velit, ut rutrum nunc semper in. Curabitur turpis tortor, sagittis nec risus sed, viverra rhoncus turpis. Aliquam mollis elit sit amet tincidunt venenatis. Proin lobortis, dolor at pulvinar tempor, libero quam pretium odio, sollicitudin vehicula ante massa eget risus. Mauris ultrices lorem et nisi porta venenatis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris eu tristique turpis. Duis ante dolor, vehicula a enim id, dapibus fermentum urna. Fusce facilisis scelerisque suscipit. In hac habitasse platea dictumst. Nulla ultrices ac ipsum quis elementum. Donec ultricies mattis placerat. Mauris nec viverra urna.';
+
+const lines = content
+  .substr(0, content.length - 1)
+  .split('. ')
+  .map((l) => l + '.');
+
 const Template: ComponentStory<typeof Provider> = (args) => (
   <Provider {...args}>
-    <Highlight content="Lorem ipsum dolor sit amet consectetur adipiscing elit." />
-    <Highlight content="Doneceleifend metus velit, ut rutrum nunc semperin." />
+    Multiple Content Elements:
+    <ul>
+      {lines.map((l) => (
+        <li>
+          <Highlight content={l} />
+        </li>
+      ))}
+    </ul>
   </Provider>
 );
 
