75cc85d6aaf04d35bbfdaeb0e6d8b6a437fc41f1
Ringo Hoffmann <riho@live.de>
2021-11-29 09:16:53 +0100

    fix provider export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Highlight } from './components/Highlight';
 export { HighlightContext } from './components/Provider';
+export { Provider } from './components/Provider';
 
 export type { GlobalProps } from './models/props';
